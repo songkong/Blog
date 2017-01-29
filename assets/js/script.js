2077a2c261833af5aac9e4c20405fe3100571abf
songkong <sosongkong@gmail.com>
2017-01-30 00:33:52 +0800

    optimize html structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,28 +12,3 @@ function display () {
 		$("#top").fadeOut(300);
     }
 }
-
-
-/* 
-* Baidu analytics
-*/
-
-var _hmt = _hmt || [];
-    (function () {
-        var hm = document.createElement("script");
-        hm.src = "//hm.baidu.com/hm.js?5b31ffbbdc6cff18727f3adf75a03777";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
-	
-/* 
-* Google analytics
-*/
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-82494501-1', 'auto');
-ga('send', 'pageview');
